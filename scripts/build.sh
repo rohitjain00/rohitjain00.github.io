a012640299542a125fca0fc3fad6e59414cb9569
Rohit Jain <rohitjain18005@gmail.com>
2020-03-08 15:43:25 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 jekyll build
-htmlproofer ./_site --file-ignore ./_site/editor.html --url-ignore "feed.xml,/facebook.com/,/freecodecamp.org/, /github.com/, /linkedin.com/"
+htmlproofer ./_site --file-ignore ./_site/editor.html --url-ignore "/www.github.com/, /www.linkedin.com/"
